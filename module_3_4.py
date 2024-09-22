e507b7e4a65b3ad05e728b3970954e2bc95177fc
K-r-e-K-e-R <san.volga034@bk.ru>
2024-09-22 19:22:25 +0500

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,19 +1,23 @@
 def single_root_words(root_word, *other_words):
     # Пустой список для подходящих слов
     same_words = []
+    # Привод корня к нижнему регистру
+    lower_root_word = root_word.lower()
 
     # Перебираем слова в other_words
     for word in other_words:
-        # Условие: если root_word содержится в word или word содержится в root_word
-        if root_word in word or word in root_word:
+        # Приводим текущее слово к нижнему регистру
+        lower_word = word.lower()
+        # Условие: если корень присутствует в слове
+        if lower_root_word in lower_word:
             same_words.append(word)  # Добавляем слово в список same_words
+
 
     return same_words  # Возвращаем список подходящих слов
 
-
 # Примеры вызова функции
-result_1 = single_root_words("книга", "книги", "журнал", "газета", "письмо")
+result_1 = single_root_words("свет", "Светильник", "проСВЕТ", "Осветленный", "Святотатсво")
 print("Подходящие слова:", result_1)
 
-result_2 = single_root_words("дом", "подъезд", "квартира", "комната", "дом")
+result_2 = single_root_words("дом", "Домино", "ДОмоВОЙ", "ДомА", "дОмиКИ")
 print("Подходящие слова:", result_2)
